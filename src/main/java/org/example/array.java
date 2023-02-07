ef41241fde6468a9dc37d6dd749c4d08f0320c57
Nixon1018 <nixon.joshwa@tringapps.com>
2023-02-07 18:06:12 +0530

    first

--- src/main/java/org/example/array.java
+++ src/main/java/org/example/array.java
@@ -59,16 +59,30 @@ class flexible {
     }
 }
 public class array {
+    private static final Logger logger = Logger.getLogger("InfoLogging");
     public static void main(String[] args) {
      int[] arr;
+     int check=1;
+     Scanner sc= new Scanner(System.in);
      flexible f=new flexible();
-     f.add(2);
-     f.add(3);
-     f.add(4);
-     f.add(5);
-     f.print();
-     f.remove();
-     f.print();
-
+     while(check==1) {
+         logger.info("1.add 2.remove 3.print 4.exit");
+         int chose=sc.nextInt();
+         if(chose==1) {
+             logger.info("Enter the element:");
+             int x=sc.nextInt();
+             f.add(x);
+         }
+         else if(chose==2) {
+             f.remove();
+         }
+         else if(chose==3) {
+             f.print();
+         }
+         else
+         {
+             check=0;
+         }
+     }
     }
 }
