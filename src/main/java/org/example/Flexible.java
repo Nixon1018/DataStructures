3c18cda49b1f2d20d275244cc06fbb19f502a1cd
Nixon1018 <123439949+Nixon1018@users.noreply.github.com>
2023-02-28 00:29:48 +0530

    Create Flexible.java

---
+++ src/main/java/org/example/Flexible.java
@@ -0,0 +1,60 @@
+package org.example;
+
+import java.util.Scanner;
+import java.util.logging.Logger;
+
+class Flexible {
+    int[] arr;
+    int size = 0;
+    private static final Logger logger = Logger.getLogger("InfoLogging");
+
+    void increase() {
+        if (size == 0) {
+
+            arr = new int[1];
+            size += 1;
+        } else {
+            int[] temp = new int[size + 1];
+            for (int i = 0; i < size; i += 1) {
+                temp[i] = arr[i];
+
+            }
+            arr = temp;
+            size += 1;
+        }
+    }
+
+    void decrease() {
+        int[] temp = new int[size - 1];
+        for (int i = 0; i < size-1; i += 1) {
+            temp[i] = arr[i];
+        }
+        arr = temp;
+
+        size--;
+    }
+
+    void add(int data) {
+        increase();
+        arr[size - 1] = data;
+    }
+
+    void remove()
+    {
+        if(size==0)
+        {
+            logger.info("The array is empty");
+        }
+        else {
+            decrease();
+        }
+    }
+    void print()
+    {
+        for(int i=0;i<size;i+=1)
+        {
+            String z=" "+arr[i];
+            logger.info(z);
+        }
+    }
+}
